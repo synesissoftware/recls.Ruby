1070cf2c4742bd5a9fe7a1c7bea85239ee64eaae
Matthew Wilson <matthew@synesis.com.au>
2012-01-13 11:56:46 +0000

    recls.ruby : added common.rb

---
+++ lib/recls/internal/common.rb
@@ -0,0 +1,15 @@
+# ######################################################################### #
+# File:		recls/internal/common.rb
+#
+# Purpose:	Common include for recls.ruby
+#
+# Created:	13th January 2012
+# Updated:	13th January 2012
+#
+# Author:	Matthew Wilson
+#
+# Copyright:	<<TBD>>
+#
+# ######################################################################### #
+
+
