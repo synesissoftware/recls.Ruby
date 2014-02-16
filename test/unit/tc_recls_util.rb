a603cf02005671fb30e2cf972958dc5157a85453
Matt Wilson <stlsoft@gmail.com>
2014-02-17 09:05:31 +1100

    + added further tests

--- test/unit/tc_recls_util.rb
+++ test/unit/tc_recls_util.rb
@@ -10,7 +10,40 @@ class Test_canonicalisePath < Test::Unit::TestCase
 		assert_equal('.', Recls::canonicalisePath('.'))
 		assert_equal('..', Recls::canonicalisePath('..'))
 
-	end
+	end # test_dots_directories
+
+
+	def test_canonicalised_directories_one_level
+
+		assert_equal('abc', Recls::canonicalisePath('abc'))
+		assert_equal('abc/', Recls::canonicalisePath('abc/'))
+		assert_equal('/abc', Recls::canonicalisePath('/abc'))
+		assert_equal('/abc/', Recls::canonicalisePath('/abc/'))
+
+	end # test_canonicalised_directories_one_level
+
+	def test_canonicalised_directories_two_levels
+
+		assert_equal('abc/def', Recls::canonicalisePath('abc/def'))
+		assert_equal('abc/def/', Recls::canonicalisePath('abc/def/'))
+		assert_equal('/abc/def', Recls::canonicalisePath('/abc/def'))
+		assert_equal('/abc/def/', Recls::canonicalisePath('/abc/def/'))
+
+	end # test_canonicalised_directories_two_levels
+
+	def test_uncanonicalised_directories_one_level
+
+		assert_equal('.', Recls::canonicalisePath('.'))
+		assert_equal('./', Recls::canonicalisePath('./'))
+		assert_equal('/.', Recls::canonicalisePath('/.'))
+		assert_equal('/./', Recls::canonicalisePath('/./'))
+
+		assert_equal('..', Recls::canonicalisePath('..'))
+		assert_equal('../', Recls::canonicalisePath('../'))
+		assert_equal('/..', Recls::canonicalisePath('/..'))
+		assert_equal('/../', Recls::canonicalisePath('/../'))
+
+	end # def test_uncanonicalised_directories_one_level
 
 end
 
