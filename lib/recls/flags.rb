33acf74dc4f9ec62f2879c6a914b0ab332c4f3f3
Matthew Wilson <matthew@synesis.com.au>
2014-10-13 19:20:16 +1100

    + comments on flags

--- lib/recls/flags.rb
+++ lib/recls/flags.rb
@@ -15,15 +15,23 @@
 
 module Recls
 
+	# Specifies that files are to be listed
 	FILES					=	0x00000001
+	# Specifies that directories are to be listed
 	DIRECTORIES				=	0x00000002
+	# Specifies that links are to be listed (and not followed)
 	LINKS					=	0x00000004
+	# Specifies that devices are to be listed
 	DEVICES					=	0x00000008
 	TYPEMASK				=	0x000000ff
 
+	# Specifies that hidden items are to be shown and hidden directories are
+	# to be searched
 	SHOW_HIDDEN				=	0x00000100
 
 	DIR_PROGRESS			=	0x00001000
+	# Causes search to terminate if a directory cannot be entered or an
+	# entry's information cannot be stat()'d
 	STOP_ON_ACCESS_FAILURE	=	0x00002000
 	LINK_COUNT				=	0000004000
 	NODE_INDEX				=	0x00008000
@@ -36,3 +44,4 @@ module Recls
 end # module Recls
 
 # ############################## end of file ############################# #
+
