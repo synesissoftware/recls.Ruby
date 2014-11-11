0384ef6205cfd74b466f6b6896dc57b2bce3ef7f
Matthew <matthew@synesis.com.au>
2014-11-12 07:02:47 +1100

    ~ reordered requires in ts_all[.rb] to reduce chance of require order dependencies being missed

--- test/unit/ts_all.rb
+++ test/unit/ts_all.rb
@@ -6,13 +6,12 @@ ThisDir = File.expand_path(File.dirname(__FILE__))
 $:.unshift File.join(File.dirname(__FILE__), '../..', 'lib')
 
 
-require 'recls'
-require 'test/unit'
-
 puts "executing all test cases in directory #{ThisDir}"
 
 require ThisDir + '/' + 'tc_recls_file_search'
 require ThisDir + '/' + 'tc_recls_module'
 require ThisDir + '/' + 'tc_recls_util'
 require ThisDir + '/' + 'tc_recls_ximpl_util'
+
+require 'test/unit'
 
