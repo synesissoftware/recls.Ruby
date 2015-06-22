d32d7b485992d52653df3e3c4e7a0ea3eb0921ec
Matthew <matthew@synesis.com.au>
2015-06-22 17:21:36 +1000

    ~ hash-bang fixes and trivial additions to scratch tests

--- test/scratch/test_entry.rb
+++ test/scratch/test_entry.rb
@@ -1,3 +1,6 @@
+#! /usr/bin/ruby
+#
+# test Recls entry methods
 
 require 'recls'
 
--- test/scratch/test_show_hidden.rb
+++ test/scratch/test_show_hidden.rb
@@ -1,3 +1,6 @@
+#! /usr/bin/ruby
+#
+# Demonstrates search of hidden files
 
 require 'recls'
 
@@ -10,7 +13,15 @@ end
 
 puts
 puts "Hidden files:"
-Recls::FileSearch.new(nil, nil, Recls::RECURSIVE | Recls::SHOW_HIDDEN).each do |fe|
+Recls::FileSearch.new(nil, nil, Recls::RECURSIVE | Recls::FILES | Recls::SHOW_HIDDEN).each do |fe|
+
+	puts "\t#{fe.searchRelativePath}" if fe.hidden?
+end
+
+
+puts
+puts "Hidden directories & files:"
+Recls::FileSearch.new(nil, nil, Recls::RECURSIVE | Recls::DIRECTORIES | Recls::FILES | Recls::SHOW_HIDDEN).each do |fe|
 
 	puts "\t#{fe.searchRelativePath}" if fe.hidden?
 end
