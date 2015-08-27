7dd5e9fcc254272c60b2d3f76b758966286a3d96
Matthew Wilson <matthew@synesis.com.au>
2015-08-27 21:06:15 +1000

    test/scratch/test_show_dev_and_ino(.rb) : + also showing links

--- test/scratch/test_show_dev_and_ino.rb
+++ test/scratch/test_show_dev_and_ino.rb
@@ -8,6 +8,6 @@ require 'recls'
 
 Recls::FileSearch.new('.', Recls::WILDCARDS_ALL, Recls::FILES | Recls::RECURSIVE).each do |fe|
 
-	puts "#{fe.search_relative_path}\t#{fe.dev}\t#{fe.ino}"
+	puts "#{fe.search_relative_path}\t#{fe.dev}\t#{fe.ino}\t#{fe.nlink}"
 end
 
