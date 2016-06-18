36c52de6b498ce2e9d622311a2d12e7a728cabdf
Matthew <matthew@synesis.com.au>
2016-06-18 10:48:46 +1000

    ~ fixed frozen string problem

--- lib/recls/version.rb
+++ lib/recls/version.rb
@@ -4,7 +4,7 @@
 # Purpose:      Version for recls library
 #
 # Created:      14th February 2014
-# Updated:      9th June 2016
+# Updated:      18th June 2016
 #
 # Author:       Matthew Wilson
 #
@@ -39,7 +39,7 @@
 module Recls
 
 	# Current version of the recls.Ruby library
-	VERSION				=	'2.7.1'
+	VERSION				=	'2.7.2'
 
 	private
 	VERSION_PARTS_		=	VERSION.split(/[.]/).collect { |n| n.to_i } # :nodoc:
